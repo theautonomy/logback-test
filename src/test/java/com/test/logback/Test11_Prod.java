caf7b9d1dc764113fcf5235a8925c68ae9498fde
WeiLi <weili.mail@gmail.com>
2018-07-17 18:27:47 -0400

    Create Test11_Prod.java

---
+++ src/test/java/com/test/logback/Test11_Prod.java
@@ -0,0 +1,27 @@
+package com.test.logback;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test11_Prod {
+
+	final static Logger logger = LoggerFactory.getLogger(Test11_Prod.class);
+
+	@Test
+	public void testLog() {
+
+		System.setProperty("logback.config.file.name", "/logback-test11-prod.xml");
+		
+		String fileName = System.getProperty("logback.config.file.name");
+
+		SLF4jConfigUtil.configureUsingClassPathResource(fileName);
+		
+		logger.debug("This is a debug level log message");
+
+		logger.info("This is an info level log message");
+		
+		logger.warn("This is a warn level log message");
+
+	}
+}
